d883bf56baeb839dc92743424f282badaeb4f895
Ubuntu <ubuntu@ip-172-31-38-119.eu-north-1.compute.internal>
2025-08-26 19:50:46 +0000

    updated base.html

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,6 @@ import os
 sys.path.insert(0, os.path.dirname(__file__))
 
 from app import app as application  # if your Flask instance is in app.py
+
+if __name__ == "__main__":
+    application.run()
