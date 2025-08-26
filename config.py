734c9fe2c607b5be0730407216cd7724951b7302
Ubuntu <ubuntu@ip-172-31-38-119.eu-north-1.compute.internal>
2025-08-26 14:22:31 +0000

    fixed db issues

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv("/home/ubuntu/Flask-Web-App/.env")
+
+DB_USER = os.getenv("RDS_USER")
+DB_PASS = os.getenv("RDS_PASSWORD")
+DB_HOST = os.getenv("RDS_HOST")
+DB_NAME = os.getenv("RDS_DBNAME")
+DB_PORT = os.getenv("RDS_PORT", 3306)
 
 class Config:
-    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "dev-secret-key")  # change in prod
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///instance/auth.db")
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "dev-secret-key")
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,6 @@
-from app import app as application
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+from app import app as application  # if your Flask instance is in app.py
